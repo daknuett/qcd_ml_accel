bb1ee5dd8015087c4d6d13cf0830fa076dd3a602
Daniel Knuettel <daniel.knuettel@phsik.uni-regensburg.de>
2024-09-12 16:13:19 +0200

    fixed a test

--- test/test_v_4pool.py
+++ test/test_v_4pool.py
@@ -18,7 +18,7 @@ def unpool(features, L_coarse, block_size):
         res[x*block_size[0]: (x + 1) * block_size[0]
             , y*block_size[1]: (y + 1) * block_size[1]
             , z*block_size[2]: (z + 1) * block_size[2]
-            , t*block_size[3]: (z + 1) * block_size[3]] = features[x,y,z,t]
+            , t*block_size[3]: (t + 1) * block_size[3]] = features[x,y,z,t]
     return res
 
 def test_v_pool4d():
@@ -37,21 +37,6 @@ def test_v_unpool4d():
     L_coarse = [2, 2, 2, 2]
     block_size = [4, 4, 4, 4]
     coarse_v = pool(fine_v, L_coarse, block_size)
-
-    new_fine_v = unpool(coarse_v, L_coarse, block_size)
-
-    unpooled = qcd_ml_accel.pool4d.v_unpool4d(coarse_v, torch.tensor(block_size, dtype=torch.uint64));
-
-    assert torch.allclose(new_fine_v, unpooled)
-
-def test_v_unpool4d_small():
-    fine_v = torch.randn(4, 4, 4, 4, 4, 3, dtype=torch.cdouble)
-    L_coarse = [2, 2, 2, 2]
-    block_size = [2, 2, 2, 2]
-    coarse_v = pool(fine_v, L_coarse, block_size)
-
-    coarse_v[:] = 0
-    coarse_v[0,0,0,0] = 1
 
     new_fine_v = unpool(coarse_v, L_coarse, block_size)
 
